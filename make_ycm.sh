e3fe424b9626471c8c133173a8b3a90e8b39650d
aswna <aswna@server.fake>
2013-06-04 15:14:15 +0200

    Make script for YCM.

---
+++ make_ycm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. make_ycm.site.conf
+echo "PATH_TO_PYTHON_LIBRARY = ${PATH_TO_PYTHON_LIBRARY}"
+echo "PATH_TO_PYTHON_INCLUDE_DIR = ${PATH_TO_PYTHON_INCLUDE_DIR}"
+echo "PATH_TO_GCC = ${PATH_TO_GCC}"
+echo "PATH_TO_LIBCLANG = ${PATH_TO_LIBCLANG}"
+
+mkdir -p ~/tmp/ycm_build &&
+cd ~/tmp/ycm_build &&
+cmake -G "Unix Makefiles" \
+      -DPYTHON_LIBRARY=${PATH_TO_PYTHON_LIBRARY}/libpython2.7.so \
+      -DPYTHON_INCLUDE_DIR=${PATH_TO_PYTHON_INCLUDE_DIR}/python2.7 \
+      -DCMAKE_CXX_COMPILER=${PATH_TO_GCC}/g++ \
+      -DCMAKE_C_COMPILER=${PATH_TO_GCC}/gcc \
+      -DEXTERNAL_LIBCLANG_PATH=${PATH_TO_LIBCLANG}/libclang.so \
+      ~/.vim/bundle/YouCompleteMe/cpp &&
+make ycm_core
