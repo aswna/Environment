212f4c804aff441a9a75e32a8bb0831614d526b1
aswna <aswna@server.fake>
2013-10-30 13:48:28 +0100

    Wait with YCM updates, since it is not really working for me.

---
+++ update_submodules_except_for_ycm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+### Pull all git submodules except for the modules described by EXCLUDE_MODULES.
+
+# Extended regular expression:
+EXCLUDE_MODULES='YouCompleteMe'
+
+execute_git_pull()
+{
+    repo_dir="$1"
+    echo
+    echo "Pull in ${repo_dir}"
+    git pull
+}
+
+update_all_submodules_in_repository()
+{
+    old_pwd=`pwd`
+    repo_dir="$1"
+    cd ${repo_dir}
+
+    execute_git_pull ${repo_dir}
+
+    if [ -r .gitmodules ]
+    then
+        echo "Found .gitmodules file..."
+        for submodule in $(sed -n 's/^\tpath = //p' .gitmodules | egrep -v "${EXCLUDE_MODULES}")
+        do
+            (update_all_submodules_in_repository "${submodule}")
+        done
+    fi
+
+    cd ${old_pwd}
+}
+
+update_all_submodules_in_repository `pwd`
