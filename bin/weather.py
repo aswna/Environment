58e112e91c5424ae4f3e124c11ea62424430fa0d
aswna <aswna@server.fake>
2020-07-02 10:04:52 +0200

    Update weather GNU screen applet

--- bin/weather.py
+++ bin/weather.py
@@ -17,11 +17,51 @@ from __future__ import print_function
 
 import argparse
 import json
+import sys
 import urllib2
 
 from weather_site import API_KEY
 
-URL_TEMPLATE = 'https://openweathermap.org/data/2.5/weather?q={}&appid={}'
+URL_TEMPLATE = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+
+
+def main():
+    args = parse_arguments()
+    degree_sign = get_degree_sign(args.no_degree_sign, args.screen)
+
+    response = None
+    try:
+        url_to_query = URL_TEMPLATE.format(args.location, API_KEY)
+        debug(args.debug, "URL = {}".format(url_to_query))
+        response = urllib2.urlopen(url_to_query)
+        debug(args.debug, "response = {}".format(response))
+    except urllib2.URLError:
+        pass
+
+    if response:
+        json_string = response.read()
+        debug(args.debug, "json string = {}".format(json_string))
+        response.close()
+
+        parsed_json = json.loads(json_string)
+        debug(args.debug, "parsed json = {}".format(parsed_json))
+        if parsed_json:
+            if 'main' in parsed_json and 'temp' in parsed_json['main']:
+                temp_c = float(parsed_json['main']['temp'])
+                debug(args.debug, "temp (C) = {}".format(temp_c))
+                weather = None
+                if 'weather' in parsed_json and parsed_json['weather'] and \
+                        'description' in parsed_json['weather'][0]:
+                    weather = parsed_json['weather'][0]['description']
+                    debug(args.debug, "weather = {}".format(weather))
+
+                string_to_display = '{:.1f}{}C'.format(temp_c, degree_sign)
+                if weather:
+                    string_to_display += ' ({})'.format(' '.join(
+                        word.capitalize() for word in weather.split()))
+                print(string_to_display)
+                return
+    print('N/A')
 
 
 def parse_arguments():
@@ -30,6 +70,9 @@ def parse_arguments():
         description=description,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-d', '--debug', action='store_true',
+        help='enable debug mode (default: %(default)s)')
     group.add_argument(
         '-s', '--screen', action='store_true',
         help='use degree sign for the buggy GNU Screen 4.00 '
@@ -55,33 +98,9 @@ def get_degree_sign(do_not_print_degree_sign, degree_sign_for_screen):
     return '\xc2\xb0'
 
 
-def main():
-    args = parse_arguments()
-    degree_sign = get_degree_sign(args.no_degree_sign, args.screen)
-
-    response = None
-    try:
-        url_to_query = URL_TEMPLATE.format(args.location, API_KEY)
-        response = urllib2.urlopen(url_to_query)
-    except urllib2.URLError:
-        pass
-
-    if response:
-        json_string = response.read()
-        parsed_json = json.loads(json_string)
-
-        temp_c = parsed_json['main']['temp']
-        weather = parsed_json['weather'][0]['description']
-
-        string_to_display = '{:.1f}{}C'.format(temp_c, degree_sign)
-        if weather:
-            string_to_display += ' ({})'.format(
-                ' '.join(word.capitalize() for word in weather.split()))
-        print(string_to_display)
-
-        response.close()
-    else:
-        print('N/A')
+def debug(do_debug, message):
+    if do_debug:
+        print(message, file=sys.stderr, end='\n')
 
 
 if __name__ == '__main__':
